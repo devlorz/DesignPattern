4d74e7285919d1eff2e9e017c462da170b1b2c15
leelorz6 <leelorz6@gmail.com>
2017-02-22 13:48:36 +0700

    Update Singleton pattern

---
+++ src/com/leeway/SingletonPattern/ChocolateBoiler.java
@@ -0,0 +1,35 @@
+package com.leeway.SingletonPattern;
+
+/**
+ * Created by DEV-PC5 on 2017/02/22.
+ */
+public class ChocolateBoiler {
+
+    private boolean empty;
+    private boolean boiled;
+
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+}
